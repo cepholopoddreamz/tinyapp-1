e07b096e1e0ba5edbb758fead576db801167c484
cepholopoddreamz <leslie.plumb@gmail.com>
2019-01-20 19:22:34 +0000

    found head error in the app get for urls 2 instances of render were being called

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var PORT = 8080; // default port 8080
+var PORT = 3000; // default port 8080
 let login = false;
 let registration = false;
 
@@ -26,28 +26,42 @@ app.get("/", (req, res) => {
   res.send("A is for Apple");
 });
 
-//remember that app.get shows, and app.post does work with new user input -- you were trying to app.post your template vars -- but that is showing a list you had on your server object. app.get is where you put this/// 
+app.get("/partials", (req, res) => {
+  res.render("partials/_head");
+}); 
+
 app.get("/urls", (req,res) => {
-  res.render("urls");
   let templateVars = { 
     listing: urlDatabase,
   };
   res.render("urls", templateVars);
 });
 
-// app.post("/urls", (req, res) => {
-//   console.log(req.body);
-  
-// });
-
-app.get("/partials", (req, res) => {
-  res.render("partials/_head");
+app.post("/urls", (req, res) => {
+  console.log(req.body);  
+  let randomindex = generateRandomString();
+  urlDatabase[randomindex] = req.body.longURL;
+  console.log(urlDatabase);
+  console.log(req.body.longURL)
+ 
+  res.redirect('/urls/'+randomindex)
 });
+
+
 
 app.get("/urls/new", (req, res) => {
  
   res.render("new");
 });
+
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  // console.log(req.params.shortURL)
+  res.redirect(longURL); /// it's not '/urls/'+ because you are going to the websit they've written
+});
+
+
+
 
 
 function generateRandomString() {
