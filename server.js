35725939817542ffc39045ca11920cd443e5b309
cepholopoddreamz <leslie.plumb@gmail.com>
2019-01-20 02:03:01 +0000

    added list of urls from urlDatabase object to url page plus small css

--- server.js
+++ server.js
@@ -22,19 +22,42 @@ var urlDatabase = {
 
 //
 
-
 app.get("/", (req, res) => {
   res.send("A is for Apple");
 });
 
+//remember that app.get shows, and app.post does work with new user input -- you were trying to app.post your template vars -- but that is showing a list you had on your server object. app.get is where you put this/// 
 app.get("/urls", (req,res) => {
   res.render("urls");
-
+  let templateVars = { 
+    listing: urlDatabase,
+  };
+  res.render("urls", templateVars);
 });
+
+
+
+// app.post("/urls", (req, res) => {
+//   console.log(req.body);
+  
+// });
 
 app.get("/partials", (req, res) => {
   res.render("partials/_head");
 });
+
+function generateRandomString() {
+  var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+  var string_length = 6;
+	var randomstring = '';
+	for (var i=0; i<string_length; i++) {
+		var rnum = Math.floor(Math.random() * chars.length);
+		randomstring += chars.substring(rnum,rnum+1);
+  }
+  return randomstring
+  //console.log(randomstring);
+  
+}
 
 
 app.listen(PORT, () => {
