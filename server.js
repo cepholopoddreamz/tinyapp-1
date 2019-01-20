d9d4bf320760f2a9a086ee7092714d21ac939aa4
cepholopoddreamz <leslie.plumb@gmail.com>
2019-01-20 00:26:01 +0000

    initial root and server setup

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+var PORT = 8080; // default port 8080
+let login = false;
+let registration = false;
+
+var cookieParser = require('cookie-parser');
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+app.use(express.static('public'));
+app.use(cookieParser())
+
+app.get("/", (req, res) => {
+  res.send("A is for Apple");
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}!`);
+});
